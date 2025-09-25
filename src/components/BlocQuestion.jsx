656e04db574ea5a83fff6dcd284c93820680c9f0
laurent972 <laurent.digitick@gmail.com>
2025-09-25 18:38:41 +0200

    display answers and validation

--- src/components/BlocQuestion.jsx
+++ src/components/BlocQuestion.jsx
@@ -7,30 +7,40 @@ export default function BlocQuestion({...props}) {
     const array = Object.entries(props.propositions);
     const [answer, setAnswer] = useState("");
     const [answered, setAnswered] = useState(false);
+    const [goodAnswerState, setGoodAnswerState] = useState(false);
 
     const handleSubmit = (e) => {
     e.preventDefault();
     const form = e.target;
-    console.log(answer);
+
     setAnswered(true);
-  
-    
+    if (answer.substring(0) === goodAnswer) {
+        console.log("Bonne réponse");
+        
+        setGoodAnswerState(true);
+         console.log(goodAnswerState);
+    }
+      
   };
 
 
 
   return (
     <>
+      {console.log(goodAnswerState)}
         <div>{question}</div>
-        <form onSubmit={handleSubmit}>
-            {array.map((clé,id) => (
-               <p key={id} className='flex gap-2'>
-                    <label >
-                        <input type="radio" name='reponse' value={clé[0]} onChange={e => setAnswer(e.target.value)}/>
-                        {clé[1]}
+        <form id={question} onSubmit={handleSubmit}>
+            {array.map((key,id) => (
+               <div key={id} className='flex gap-2'>
+                    <input id={key[0] + key[1]}  className="hidden peer" type="radio" name='reponse' value={key[0]} onChange={e => setAnswer(e.target.value)} required/>
+                    <label htmlFor={key[0] + key[1]}  className="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 dark:peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700" >
+                       <p className="block">
+                        {key[1]}
+                        </p>
                     </label>
-                </p>
+                </div>
             ))}
+             
              <button className="border border-solid border-black px-4 py-2 mt-4">
               Valider
             </button>
